f9a198e0628351aa04d7775b96b78dc06ff1189f
Alex Huynh <alexh3925@gmail.com>
2025-09-04 23:37:55 -0700

    feat: added profile picture

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,29 @@
-import { Sidebar, ProjectGallery, SectionTitle } from "@/components";
-import { projects } from "@/data/projects";
+import Image from "next/image";
+import { Sidebar, SectionTitle } from "@/components";
 
 export default function About() {
   return (
     <section className="p-6">
       <Sidebar />
 
-      <SectionTitle title="Project Dex" variant="B" proportion={1} />
-
-      <ProjectGallery projectList={projects} />
+      <div className="max-w-5xl mx-auto flex flex-col md:flex-row-reverse justify-center items-center gap-30">
+        <div className="relative w-1/2 aspect-[5/7] flex justify-center items-center">
+          <div className={`absolute inset-0 -m-[10px] bg-slate-300 [clip-path:polygon(8%_8%,92%_10%,90%_90%,10%_92%)] -z-40`} />
+          <div className="relative w-4/5 aspect-[5/7]">
+            <Image
+              src="/portrait.jpg"
+              alt="portrait"
+              fill
+              className="object-cover border-4 border-black -z-30"
+            />
+          </div>
+          <div className="absolute bottom-20 w-full h-20">
+            <SectionTitle title="Alex Huynh" variant="B" proportion={3} type="main" />
+          </div>
+        </div>
+        <div className="relative w-1/2 aspect-[5/7]">
+        </div>
+      </div>
     </section>
   );
 }
