a5a1c7cd0cb13537daf262977ab3ae3801dc22ea
Alex Huynh <alexh3925@gmail.com>
2025-09-14 19:20:49 -0700

    feat: added about me

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,12 +9,12 @@ export default function About() {
     <section className="p-6 lg:pl-64">
       <Sidebar />
 
-      <div className="max-w-5xl mx-auto flex flex-col md:flex-row-reverse justify-center items-center gap-5">
-        <div className="w-full md:w-9/20 aspect-[5/7] flex flex-col justify-center items-center gap-2">
+      <div className="max-w-md mx-auto flex justify-center mb-15">
+        <div className="w-full aspect-[5/7] flex flex-col justify-center items-center gap-2">
           <div className="relative w-full h-full">
             <div className={`absolute inset-0 -m-[10px] bg-slate-300 [clip-path:polygon(8%_8%,92%_9%,91%_91%,9%_92%)] -z-20`} />
             <div className={`absolute inset-0 bg-slate-200 [clip-path:polygon(8%_8%,92%_9%,91%_91%,9%_92%)] -z-10 flex items-center justify-center`}>
-              <div className="relative w-7/10 md:w-4/5 aspect-[5/7]">
+              <div className="relative w-4/5 aspect-[5/7]">
                 <Image
                   src="/about/portrait.jpg"
                   alt="portrait"
@@ -43,17 +43,22 @@ export default function About() {
             ))}
           </div>
         </div>
-        <div className="w-full md:w-11/20 aspect-[5/7] flex flex-col justify-center items-center gap-2">
-          <div className="relative w-full h-full">
-            <div className={`absolute inset-0 -m-[10px] bg-slate-300 [clip-path:polygon(8%_8%,92%_9%,91%_91%,9%_92%)] -z-20`} />
-            <div className={`absolute inset-0 bg-slate-200 [clip-path:polygon(8%_8%,92%_9%,91%_91%,9%_92%)] -z-10 flex items-center justify-center`}>
-              <div className="relative w-4/5 aspect-[5/7]">
-              </div>
-            </div>
-            <div className="absolute top-5 left-5 w-3/5 z-10 -rotate-5">
-              <SectionTitle title="About Me" variant="B" proportion={3} type="main" />
-            </div>
-          </div>
+      </div>
+
+      <div className="relative max-w-5xl mx-auto h-120 my-30">
+        <div className="absolute right-10 -top-15 w-80 z-10">
+          <SectionTitle title="About Me" variant="B" proportion={3} type="main" />
+        </div>
+        <div className={`absolute inset-0 -m-[10px] bg-slate-300 [clip-path:polygon(0_0,100%_5%,100%_95%,0%_100%)] -z-20`} />
+        <div className={`absolute inset-0 bg-slate-200 [clip-path:polygon(0_0,100%_5%,100%_95%,0%_100%)] -z-10`} />
+        <div className="absolute inset-0 text-black flex justify-center items-center px-8">
+          <ul className="list-none list-inside mt-4">
+            {aboutMe.map((about) => (
+              <li className="text-xs sm:text-sm md:text-md xl:text-lg mb-4">
+                <span className="font-semibold font-rajdhani">{about}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
 
--- src/data/about.ts
+++ src/data/about.ts
@@ -1,33 +1,35 @@
 export interface Social {
-    name: string;
-    link: string;
-    imageUrl: string;
+  name: string;
+  link: string;
+  imageUrl: string;
 }
 export const socials: Social[] = [
-    {
-        name: "LinkedIn",
-        link: "https://www.linkedin.com/in/alexhuynh35",
-        imageUrl: "/svgs/linkedin.svg",
-    },
-    {
-        name: "GitHub",
-        link: "https://github.com/AlexHuynh35",
-        imageUrl: "/svgs/github.svg",
-    },
-    {
-        name: "Itch",
-        link: "https://humanbean35.itch.io",
-        imageUrl: "/svgs/itch.svg",
-    },
-    {
-        name: "Tableau",
-        link: "https://public.tableau.com/app/profile/alex.huynh7622",
-        imageUrl: "/svgs/tableau.svg",
-    },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/alexhuynh35",
+    imageUrl: "/svgs/linkedin.svg",
+  },
+  {
+    name: "GitHub",
+    link: "https://github.com/AlexHuynh35",
+    imageUrl: "/svgs/github.svg",
+  },
+  {
+    name: "Itch",
+    link: "https://humanbean35.itch.io",
+    imageUrl: "/svgs/itch.svg",
+  },
+  {
+    name: "Tableau",
+    link: "https://public.tableau.com/app/profile/alex.huynh7622",
+    imageUrl: "/svgs/tableau.svg",
+  },
 ]
 
 export const aboutMe: string[] = [
-    "",
-    "",
-    "",
-]
+  "Welcome to my portfolio! I'm excited to share my journey with you.",
+  "I've always been passionate about gaming and creating; in fact, my favorite game of all time is Minecraft because of its limitless creativity. This passion led me to pursue a career in the gaming industry, where I can use my technical capabilities and analytical thinking to make and improve experiences for gamers.",
+  "Right now, I'm an indie game developer at Origo Studios, a small team founded by me and my friends, where we're currently developing a top-down action game called Swarm2D. Being a part of the team has allowed to continuously learn and grow as a game designer, and I am very grateful for that.",
+  "Outside of Origo, I also work on projects that strengthen my skills in data analysis and software engineering. I also play a lot of games since they're my primary source of inspiration for everything I do.",
+  "I would say my greatest strength is problem-solving. I'm very proactive at identifying flaws and coming up with solutions. On teams, I'm always looking for ways to improve processes and make my teammates' work smoother and more enjoyable."
+];
